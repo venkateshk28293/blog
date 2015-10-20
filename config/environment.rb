e1d44deaec381172749949410b2d843dfd75b795
Venkatesh <venkateshk28293@gmail.com>
2015-10-20 17:03:19 +0530

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+# ENV['INLINEDIR'] = RAILS_ROOT + "/tmp"
+# Initialize the Rails application.
+Rails.application.initialize!
